09774940a58d26fc476a71d86d320a1eaaee12e3
MarkAlexI <markovaleksandr108@gmail.com>
2024-06-11 00:26:15 +0300

    Added sidebar to the main page. Need implemented functionality.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
+import Sidebar from "./components/Sidebar";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <p>
-          Get started!
-        </p>
-      </div>
+    <main className="flex h-full">
+      <Sidebar />
     </main>
   )
 }
