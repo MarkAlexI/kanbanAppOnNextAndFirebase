d9389d14f0b0e77a457a06f5f574e35e7dee74f9
MarkAlexI <markovaleksandr108@gmail.com>
2024-06-24 00:52:42 +0300

    Added modal on the "Edit board" button in the dropdown.

--- src/app/components/Dropdown.tsx
+++ src/app/components/Dropdown.tsx
@@ -1,8 +1,13 @@
+import { openAddAndEditBoardModal } from "../../redux/features/appSlice";
+import { useAppDispatch } from "@/components/redux/hooks";
+
 interface IDropdown {
   show: boolean;
 }
 
 export default function Dropdown({ show }: IDropdown) {
+  const dispatch = useAppDispatch();
+
   return (
     <div
       className={`${
@@ -10,7 +15,9 @@ export default function Dropdown({ show }: IDropdown) {
       } w-48 absolute top-full bg-white border shadow-lg right-0 py-2 rounded-2xl`}
     >
       <div className="hover:bg-gray-300">
-        <button className="text-sm px-4 py-2">Edit Board</button>
+        <button
+          onClick={() => dispatch(openAddAndEditBoardModal("Edit Board"))}
+          className="text-sm px-4 py-2">Edit Board</button>
       </div>
       <div className="hover:bg-gray-300">
         <button className="text-sm px-4 py-2">Delete Board</button>
